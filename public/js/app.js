9182470069980cbb69bc1f6bf49fbb18e56f627f
Ankit Kumar <ankit.kumar@forwardcode.in>
2023-11-08 11:22:59 +0530

    updated the app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 console.log("app.js");
 const closeButton = document.getElementById('flash-close');
 const flashBody = document.getElementById('flash-body');
-closeButton.addEventListener('click', () => {
-  flashBody.style.display = 'none';
-}, false)
+if (closeButton != null) {
+  closeButton.addEventListener('click', () => {
+    flashBody.style.display = 'none';
+  }, false)
+}

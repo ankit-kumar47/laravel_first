807dde84a2778c5b1e1875d1152667585e302228
Ankit Kumar <ankit.kumar@forwardcode.in>
2023-11-08 11:22:17 +0530

    updated the view

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,2 +1,3 @@
-<a href="{{ env('APP_URL') }}" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
+<a href="{{ back()->getTargetUrl() }}" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i>
+    Back
 </a>
--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -2,7 +2,8 @@
 <x-card class="p-6">
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block"
-            src={{ $listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.png') }} alt="" />
+            src={{ $listing->logo ? (str_contains($listing->logo, 'logos/') ? asset('storage/' . $listing->logo) : $listing->logo) : asset('images/no-image.png') }}
+            alt="" />
         <div>
             <h3 class="text-2xl hover:underline decoration-wavy">
                 <a href="listing/{{ $listing->id }}">{{ $listing->title }}</a>

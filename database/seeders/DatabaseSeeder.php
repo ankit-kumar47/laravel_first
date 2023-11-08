812d006dc415affba671170f869e07156f96b86c
Ankit Kumar <ankit.kumar@forwardcode.in>
2023-11-08 11:22:49 +0530

    Updated the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    Listing::factory(5)->create();
+    $user = User::factory()->create([
+      "name" => 'guest',
+      'email' => 'guest@mail.com',
+    ]);
+    Listing::factory(10)->create([
+      'user_id' => $user->id
+    ]);
   }
 }
